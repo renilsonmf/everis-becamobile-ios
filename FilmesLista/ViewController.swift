3fd1177b7ed5b1da99dba1dc489891bb1d59b4c7
Renilson <renilson.moreiraf@gmail.com>
2021-03-29 23:52:44 -0300

    Ajustando o layout da colectionView

--- FilmesLista/ViewController.swift
+++ FilmesLista/ViewController.swift
@@ -14,10 +14,12 @@ import AlamofireImage
 
 
 
-class ViewController: UIViewController, UICollectionViewDataSource{
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     
     // MARK: - Outlets
     @IBOutlet weak var colecaoDeFilmes: UICollectionView!
+  
+    
     
     // MARK: URL da requisição
     final let url = URL(string: "https://api.themoviedb.org/3/trending/all/week?api_key=8b26d70a68f1379627029b51b9dc87c5&language=pt-BR")
@@ -25,8 +27,10 @@ class ViewController: UIViewController, UICollectionViewDataSource{
    private var results = [Result]()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         colecaoDeFilmes.dataSource = self
+        colecaoDeFilmes.delegate = self
         makeRequest()
 
     }
@@ -68,8 +72,10 @@ class ViewController: UIViewController, UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
          let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as? PacoteFilmesCollectionViewCell
-        celulaPacote?.backgroundColor = UIColor.blue
-        celulaPacote?.layer.cornerRadius = 8
+        celulaPacote?.backgroundColor = UIColor.gray
+        celulaPacote?.layer.cornerRadius = 7
+        celulaPacote?.layer.borderWidth = 0.5
+        celulaPacote?.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
         
         if results[indexPath.row].title == nil{
             celulaPacote?.tituloLabel.text = results[indexPath.row].name
@@ -77,6 +83,11 @@ class ViewController: UIViewController, UICollectionViewDataSource{
             celulaPacote?.tituloLabel.text = results[indexPath.row].title
         }
         return celulaPacote!
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layot collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula-15, height: 160)
     }
     
     }
